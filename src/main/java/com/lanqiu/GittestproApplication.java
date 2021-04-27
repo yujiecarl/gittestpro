43426004bfc44dd05b1a81a8155272041a6eab26
carl <carl@163.com>
2021-04-27 11:14:22 +0800

    测试 1

---
+++ src/main/java/com/lanqiu/GittestproApplication.java
@@ -0,0 +1,13 @@
+package com.lanqiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lanqiu/GittestproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanqiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
